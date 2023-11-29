9cf3573fd072b9fb8d8e621c2ff5f8a26bfb519b
Marjoleine <marjegama@gmail.com>
2023-11-29 22:00:35 +0800

    heyow

---
+++ profile.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickName:'Jolyn',
+      name: {
+        firstName:'Marjoleine',
+        lastName:'Egama',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'Badowix',
+      name: {
+        firstName:'Rea Angela',
+        lastName:'Vale',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickName:'Shen',
+      name: {
+        firstName:'Shienna Rose',
+        lastName:'Hamili',
+      },
+      course:'BSN',
+      year:'3',
+    },
+    {
+      nickName:'Eker',
+      name: {
+        firstName:'Dejay',
+        lastName:'Adaya',
+      },
+      course:'BSMT',
+      year:'<3',
+    },
+    {
+      nickName:'Bing',
+      name: {
+        firstName:'Robbie Brian',
+        lastName:'Cagumbay',
+      },
+      course:'BSCS',
+      year:'1',
+    },
+  ];
